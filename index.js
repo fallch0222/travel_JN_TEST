adf0e3d0bf95b246e4378e6beda667250182b73e
fallch0222 <56762890+fallch0222@users.noreply.github.com>
2022-08-11 14:00:52 +0900

    edit

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const resultContext = document.getElementById("result context");
+const inputlocation = document.getElementById("locationInput");
 
 const api = () => {
+    console.log(inputlocation);
     var xhr = new XMLHttpRequest();
     var url = 'http://apis.data.go.kr/6460000/tourInfo/getTourlnfoList'; /*URL*/
     var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'tGzRTe4hFkBsplxnkyKO%2B5HiRgZxgHiEz1wTuNEI8oVrZxwK1Cly6McXVSTOiWjzu9S5v8sw02vxmLTnVXDA8g%3D%3D'; /*Service Key*/
-    queryParams += '&' + encodeURIComponent('tourArea') + '=' + encodeURIComponent('목포'); /**/
+    queryParams += '&' + encodeURIComponent('tourArea') + '=' + encodeURIComponent(inputlocation); /**/
     queryParams += '&' + encodeURIComponent('tourCategory') + '=' + encodeURIComponent('관광'); /**/
-    queryParams += '&' + encodeURIComponent('pageSize') + '=' + encodeURIComponent('10'); /**/
+    queryParams += '&' + encodeURIComponent('pageSize') + '=' + encodeURIComponent('100'); /**/
     queryParams += '&' + encodeURIComponent('startPage') + '=' + encodeURIComponent('1'); /**/
-    queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
+    queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /**/
     xhr.open('GET', url + queryParams);
     xhr.onreadystatechange = function () {
         if (this.readyState == 4) {
@@ -16,14 +18,13 @@ const api = () => {
             result = result.getElementsByTagName("list");
 
             for(let multiResult of result){
-                const NewElement = document.createElement('h2');
                 for(let singleResult of multiResult.children){
                     if(singleResult.innerHTML != "-"){
-                        NewElement.innerHTML += singleResult.innerHTML + "<br>";
+                        resultContext.innerHTML += singleResult.innerHTML + "<br>";
                         console.log(singleResult);
                     }   
                 }
-                NewElement.innerHTML += "<br>";
+                resultContext.innerHTML += "<br>";
             }
             
         }
@@ -36,4 +37,3 @@ const api = () => {
 
 
 
-api();
